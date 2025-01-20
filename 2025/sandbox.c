56ca2e93666c9d77c9e5e081e2b539dbef9feafb
Floriano Albertini <floriano.webdev@gmail.com>
2025-01-20 15:53:38 +0100

    Done sandbox

--- 2025/sandbox.c
+++ 2025/sandbox.c
@@ -6,7 +6,7 @@
 /*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 14:21:10 by falberti          #+#    #+#             */
-/*   Updated: 2025/01/20 14:21:11 by falberti         ###   ########.fr       */
+/*   Updated: 2025/01/20 15:53:22 by falberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,3 +41,59 @@ If verbose is true, you must write the appropriate message among the following:
 
 You must not leak processes (even in zombie state, this will be checked using
 wait).
+*/
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <signal.h>
+
+int g_pid;
+bool g_tout = false;
+
+void alarm_handler(int sig){
+    (void)sig;
+    g_tout = true;
+    kill(g_pid, SIGKILL);
+}
+
+int	sandbox(void (*f)(void), unsigned int timeout, bool verbose)
+{
+    struct sigaction sa;
+    sa.sa_handler = &alarm_handler;
+    sa.sa_flags = SA_RESTART;
+    sigemptyset(&sa.sa_mark);
+    sigaction(SIGALRM, &sa, NULL);
+    g_pid = fork();
+    if (g_pid = -1) {return (-1);}
+    if (g_pid == 0){
+        sa.sa_handler = SIG_DFL;
+        f();
+        exit(0);
+    } else {
+        alarm(timeout);
+        int wstatus;
+        if (waitpid(g_pid, &wstatus, 0) == -1){return (-1);} 
+        if (g_tout) {
+            if (verbose) {printf("Timeouted: %d\n", timeout);} return (1);
+        }
+         if (WIFEXITED(wstatus)) {
+            if(WEXITSTATUS(wstatus) == 0){ if(verbose){printf("Exited normally");}return (0);}
+            if(WEXITSTATUS(wstatus)){ if(verbose) {printf("Exited with status %d\n", WEXITSTATUS(wstatus));} return (1);}
+        } else if (WIFSIGNALED(wstatus)){
+            if (verbose) {printf("Terminated by signal %d", WTERMSIG(wstatus));}
+            return (1);
+        }
+    }
+    return (0);
+}
+
+void f_test(){
+    printf("f_test\n");
+    sleep(5);
+}
+
+int main(){
+    sandbox(f_test, 3, false);
+    return 0;
+}
